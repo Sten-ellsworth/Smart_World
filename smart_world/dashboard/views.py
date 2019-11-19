45d482cc8ca63555bd6973a06959edd94b1cafc7
Sten Ellsworth <s1118010@student.windesheim.nl>
2019-11-19 10:21:08 +0100

    made a new query in views to get all the availbale places in the parking spots, updated the index.html with the new query

--- smart_world/dashboard/views.py
+++ smart_world/dashboard/views.py
@@ -3,8 +3,11 @@ from .models import Sensors
 
 
 def index(request):
+    # this value gets all of the data out off the database
     sensors = Sensors.objects.all()
-    return render(request, "index.html", {'sensors': sensors})
+    # this value gets all of empty values out off the database
+    empty_or_full_value = Sensors.objects.filter(sensorvalue=1)
+    return render(request, "index.html", {'sensors': sensors, 'empty_or_full_value': empty_or_full_value})
 
 
 def example(request):
