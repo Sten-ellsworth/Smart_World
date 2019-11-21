0980faf6a2136f9435d4d0ac80898a3ca31ace24
Bavlly Fanous <bavlly@backbase.com>
2019-11-21 15:51:32 +0100

    API_Sensor_Database gefixt. Hierbij geen rechtstreekse data naar database, maar eerst via api mbv json

--- smart_world/dashboard/models.py
+++ smart_world/dashboard/models.py
@@ -1,3 +1,19 @@
+import datetime
+
+from django.db.models import Model, NOT_PROVIDED, DateTimeField
 from django.db import models
 
 # Create your models here.
+class sensorData(models.Model):
+    sensorValues = models.IntegerField()
+    created_at = models.DateTimeField(auto_now=True)
+    sensor_ID = models.IntegerField()
+
+    def save(self):
+        if self.created_at == None:
+            self.created_at = datetime.now()
+            super(sensorData, self).save()
+
+class sensors(models.Model):
+    sensor_ID = models.IntegerField()
+    sensorValue = models.IntegerField()
--- smart_world/dashboard/views.py
+++ smart_world/dashboard/views.py
@@ -1,5 +1,83 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import sensorData, sensors
+from .serializer import sensorDataSerializer, sensorsSerializer
 
 
 def index(request):
     return render(request, "index.html")
+
+
+@api_view(['GET'])
+def getList(request):
+    sensor_data = sensorData.objects.all()
+    serializer = sensorDataSerializer(sensor_data, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT'])
+def detailList(request, pk):
+    try:
+        sensor_data = sensorData.objects.get(pk=pk)
+    except sensorData.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = sensorDataSerializer(sensor_data)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = sensorDataSerializer(sensor_data, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def postList(request):
+    if request.method == 'POST':
+        serializer = sensorDataSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+@api_view(['GET'])
+def sensorList(request):
+    sensor = sensors.objects.all()
+    serializer = sensorsSerializer(sensor, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT'])
+def sensorDetail(request, pk):
+    try:
+        sensor = sensors.objects.get(pk=pk)
+    except sensors.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = sensorsSerializer(sensor)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = sensorDataSerializer(sensor, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def sensorPost(request):
+    if request.method == 'POST':
+        serializer = sensorDataSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
