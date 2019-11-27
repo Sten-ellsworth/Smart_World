9848397f4b88b264b3824913dfbdc67e22b4d58a
Sten Ellsworth <s1118010@student.windesheim.nl>
2019-11-27 13:04:40 +0100

    backend

---
+++ sensor_backend/lib/apiconnection.py
@@ -0,0 +1,34 @@
+import urequests 
+import sys
+import time
+import json
+import machine
+
+
+class ApiConnection:
+  
+    def __init__(self, sensor_value, sensor):
+        
+        self.distance = sensor_value
+        self.sensor = sensor
+
+
+    def addSensorValue(self):
+        if self.distance < 15: 
+          sensor_data = {
+          "sensor_ID": self.sensor,
+          "sensorValue": 0,
+            }
+          r = urequests.post('http://172.20.10.2:8080/sensor_data/post/', json=sensor_data)
+          r.close()
+          put = urequests.put('http://172.20.10.2:8080/sensor/put/'+ str(self.sensor)+ '/', json=sensor_data)
+          put.close()
+        else: 
+          sensor_data = {
+          "sensor_ID": self.sensor,
+          "sensorValue": 1,
+            }
+          r = urequests.post('http://172.20.10.2:8080/sensor_data/post/', json=sensor_data)
+          r.close()
+          put = urequests.put('http://172.20.10.2:8080/sensor/put/'+ str(self.sensor)+ '/', json=sensor_data)
+          put.close()
