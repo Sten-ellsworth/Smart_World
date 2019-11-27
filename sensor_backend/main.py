7f5abbfd2d7670d14f1d48f53e9f3d4c1ec97a3e
Sten Ellsworth <s1118010@student.windesheim.nl>
2019-11-27 13:00:27 +0100

    sensor backend

--- sensor_backend/main.py
+++ sensor_backend/main.py
@@ -4,26 +4,25 @@ import time
 import machine
 from machine import Pin, Timer
 from hcsr04 import HCSR04
+from apiconnection import ApiConnection
 import time
 import _thread
 
-sensor1 = HCSR04(Pin.exp_board.G8, Pin.exp_board.G7)
-# sensor2 = HCSR04(Pin.exp_board.G16, Pin.exp_board.G15)
+sensorconnection1 = HCSR04(Pin.exp_board.G8, Pin.exp_board.G7)
+# sensorconnection2 = HCSR04(Pin.exp_board.G16, Pin.exp_board.G15)
 
 
 def print_distance():
     while True:
-        if sensor1.distance_cm() < 15: 
-        sensordata = '0'
-        sensor = '1'
-        r = urequests.post('http://172.20.10.2:8080/Parkeergarage/get.php?value='+sensordata+'&sensor='+sensor)
-    else: 
-        sensordata = '1'
-        sensor = '1'
-        r = urequests.post('http://172.20.10.2:8080/Parkeergarage/get.php?value='+sensordata+'&sensor='+sensor)
-    r.close()
-        print("sensor1:", sensor1.distance_cm())
-        time.sleep(2)
+        print("sensor1:", sensorconnection1.distance_cm())
+        sensorApi1 = ApiConnection(sensorconnection1.distance_cm(), 1)
+        sensorApi2 = ApiConnection(sensorconnection1.distance_cm(), 2)
+
+
+        sensorApi1.addSensorValue()
+        sensorApi2.addSensorValue()
+        
+        time.sleep(10)
     
 
 # th = _thread.start_new_thread(print_distance, ())
