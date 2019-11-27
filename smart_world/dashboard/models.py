ae1292e726cf57f568026df357e51c4a90491080
Bavlly Fanous <bavlly@backbase.com>
2019-11-27 15:48:48 +0100

    Documented the code

--- smart_world/dashboard/models.py
+++ smart_world/dashboard/models.py
@@ -17,10 +17,6 @@ class SensorData(models.Model):
             self.created_at = datetime.now()
             super(SensorData, self).save(*args, **kwargs)
 
-    # x = '2014-09-23T18:43:26.692Z'
-    # y = dateparse.parse_datetime(x)
-
-
 class Sensors(models.Model):
     sensor_ID = models.IntegerField()
     sensorValue = models.IntegerField()
