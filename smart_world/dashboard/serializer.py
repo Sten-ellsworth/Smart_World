cee96f8b3376538495bfb3f1b598d0aeef2c1a55
Bavlly Fanous <bavlly@backbase.com>
2019-12-12 10:09:03 +0100

    Refactor code with documentation

--- smart_world/dashboard/serializer.py
+++ smart_world/dashboard/serializer.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import SensorData, Sensors, Graph
 
+
+# create here your serializer to read as JSON
 class SensorDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = SensorData
@@ -17,4 +19,3 @@ class GraphSerializer(serializers.ModelSerializer):
     class Meta:
         model = Graph
         fields = '__all__'
-
